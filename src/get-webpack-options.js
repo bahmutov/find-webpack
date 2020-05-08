1a7b095f59ee864c47ff0397d9988bae0e9ef3e8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-08 08:50:01 -0400

    fix: debug message about package.json, closes #6

--- src/get-webpack-options.js
+++ src/get-webpack-options.js
@@ -1,6 +1,7 @@
 // @ts-check
 const debug = require('debug')('find-webpack')
 const path = require('path')
+const fs = require('fs')
 const findYarnWorkspaceRoot = require('find-yarn-workspace-root')
 const mockEnv = require('mocked-env')
 
@@ -11,7 +12,7 @@ const tryLoadingWebpackConfig = (webpackConfigPath) => {
   // @ts-ignore
   const restoreEnv = mockEnv({
     BABEL_ENV: envName,
-    NODE_ENV: envName
+    NODE_ENV: envName,
   })
   try {
     let webpackOptions = require(webpackConfigPath)
@@ -23,6 +24,7 @@ const tryLoadingWebpackConfig = (webpackConfigPath) => {
   } catch (err) {
     debug('could not load react-scripts webpack')
     debug('error %s', err.message)
+    debug(err)
     restoreEnv()
   }
 }
@@ -51,31 +53,51 @@ const tryRootProjectWebpack = () => {
 }
 
 const tryReactScripts = () => {
-  const webpackConfigPath = path.resolve(
-    findYarnWorkspaceRoot() || process.cwd(),
+  // try requiring the file or, if it does not work, try parent folders
+  // maybe it is a monorepo situation
+  const root = findYarnWorkspaceRoot() || process.cwd()
+  debug('trying filename for react scripts from root %s', root)
+  const filename = path.resolve(
+    root,
     'node_modules',
     'react-scripts',
     'config',
     'webpack.config.js',
   )
-
-  debug('path to react-scripts own webpack.config.js: %s', webpackConfigPath)
-  return tryLoadingWebpackConfig(webpackConfigPath)
+  return tryLoadingWebpackConfig(filename)
 }
 
 const tryEjectedReactScripts = () => {
-  const webpackConfigPath = path.resolve(process.cwd(), 'config', 'webpack.config.js')
+  const webpackConfigPath = path.resolve(
+    process.cwd(),
+    'config',
+    'webpack.config.js',
+  )
   return tryLoadingWebpackConfig(webpackConfigPath)
 }
 
 /**
  * Try loading React scripts webpack config using "require" - because maybe
  * the `react-scripts` were installed in a parent folder (but without using Yarn workspace)
-*/
+ */
 const tryWebpackInReactScriptsModule = () => {
   const webpackConfigModuleName = 'react-scripts/config/webpack.config.js'
-  debug('trying to require webpack.config.js: %s', webpackConfigModuleName)
-  return tryLoadingWebpackConfig(webpackConfigModuleName)
+  debug(
+    'trying to require webpack config via path: %s',
+    webpackConfigModuleName,
+  )
+
+  const found = tryLoadingWebpackConfig(webpackConfigModuleName)
+  if (!found) {
+    debug('Could not find react-scripts webpack config')
+    const packageJsonExists = fs.existsSync(
+      path.join(process.cwd(), 'package.json'),
+    )
+    if (!packageJsonExists) {
+      debug('⚠️ react-scripts requires package.json file')
+      debug('We could not find it in % s', process.cwd())
+    }
+  }
 }
 
 /**
@@ -83,7 +105,9 @@ const tryWebpackInReactScriptsModule = () => {
  * and load it using development environment name.
  */
 const getWebpackOptions = () => {
-  const webpackOptions = tryReactScripts() ||
+  debug('get webpack starting from cwd: %s', process.cwd())
+  const webpackOptions =
+    tryReactScripts() ||
     tryEjectedReactScripts() ||
     tryVueCLIScripts() ||
     tryRootProjectWebpack() ||
