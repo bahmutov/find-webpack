87958f8504dffb5a23bf1e6d70226a013cc53af5
Sergey Kozlov <dartess@mail.ru>
2022-08-12 16:39:48 +0300

    add test for getWebpackOptions/ejectedReactScripts

--- tests/getWebpackOptions/ejectedReactScripts/findwebpack.test.js
+++ tests/getWebpackOptions/ejectedReactScripts/findwebpack.test.js
@@ -2,7 +2,7 @@ const { test } = require('uvu');
 const assert = require('uvu/assert');
 const { getWebpackOptions } = require('../../../src/index');
 
-test('getWebpackOptions / tryReactScripts', () => {
+test('getWebpackOptions / tryEjectedReactScripts', () => {
     assert.ok(getWebpackOptions());
 });
 
